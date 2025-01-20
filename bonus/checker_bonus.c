77f9f14bb70029ed3bac18960154499c53e14188
Caroline Yamashita Caleguer <cayamash@student.42sp.org.br>
2025-01-20 14:21:30 -0300

    ajustes lib

--- bonus/checker_bonus.c
+++ bonus/checker_bonus.c
@@ -6,47 +6,52 @@
 /*   By: cayamash <cayamash@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 16:45:05 by cayamash          #+#    #+#             */
-/*   Updated: 2025/01/20 10:52:48 by cayamash         ###   ########.fr       */
+/*   Updated: 2025/01/20 14:09:27 by cayamash         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/checker.h"
 
-void	exec_command(t_stack **stack_a, t_stack **stack_b, char *command)
+int	exec_command(t_stack **stack_a, t_stack **stack_b, char *command)
 {
-	if (ft_strncmp(command, "rrr", 3) == 0)
+	if (ft_strncmp(command, "rrr", 3) == 0 && ft_strlen(command) == 4)
 		reverse_rotate(stack_a, stack_b);
-	else if (ft_strncmp(command, "rra", 3) == 0)
+	else if (ft_strncmp(command, "rra", 3) == 0 && ft_strlen(command) == 4)
 		reverse_rotate(stack_a, NULL);
-	else if (ft_strncmp(command, "rrb", 3) == 0)
+	else if (ft_strncmp(command, "rrb", 3) == 0 && ft_strlen(command) == 4)
 		reverse_rotate(stack_b, NULL);
-	else if (ft_strncmp(command, "rr", 2) == 0)
+	else if (ft_strncmp(command, "rr", 2) == 0 && ft_strlen(command) == 3)
 		rotate(stack_a, stack_b);
-	else if (ft_strncmp(command, "ra", 2) == 0)
+	else if (ft_strncmp(command, "ra", 2) == 0 && ft_strlen(command) == 3)
 		rotate(stack_a, NULL);
-	else if (ft_strncmp(command, "rb", 2) == 0)
+	else if (ft_strncmp(command, "rb", 2) == 0 && ft_strlen(command) == 3)
 		rotate(stack_b, NULL);
-	else if (ft_strncmp(command, "pa", 2) == 0)
+	else if (ft_strncmp(command, "pa", 2) == 0 && ft_strlen(command) == 3)
 		push(stack_b, stack_a);
-	else if (ft_strncmp(command, "pb", 2) == 0)
+	else if (ft_strncmp(command, "pb", 2) == 0 && ft_strlen(command) == 3)
 		push(stack_a, stack_b);
-	else if (ft_strncmp(command, "sa", 2) == 0)
+	else if (ft_strncmp(command, "sa", 2) == 0 && ft_strlen(command) == 3)
 		swap(stack_a);
-	else if (ft_strncmp(command, "sb", 2) == 0)
+	else if (ft_strncmp(command, "sb", 2) == 0 && ft_strlen(command) == 3)
 		swap(stack_b);
+	else
+		return (1);
+	return (0);
 }
 
-void	read_commands(t_stack **stack_a, t_stack **stack_b)
+int	read_commands(t_stack **stack_a, t_stack **stack_b)
 {
 	char	*command;
 
 	command = get_next_line(0);
 	while (command)
 	{
-		exec_command(stack_a, stack_b, command);
+		if (exec_command(stack_a, stack_b, command))
+			return (1);
 		free(command);
 		command = get_next_line(0);
 	}
+	return (0);
 }
 
 void	checker(char **args)
@@ -56,10 +61,9 @@ void	checker(char **args)
 
 	stack_a = stack_create(args);
 	stack_b = NULL;
-	if (is_sorted(stack_a))
-		return ;
-	read_commands(&stack_a, &stack_b);
-	if (is_sorted(stack_a) && stack_size(stack_b) == 0)
+	if (read_commands(&stack_a, &stack_b))
+		ft_printf("Error\n");
+	else if (is_sorted(stack_a) && stack_size(stack_b) == 0)
 		ft_printf("OK\n");
 	else
 		ft_printf("KO\n");
---
+++ lib/ft_strjoin.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cayamash <cayamash@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/18 11:27:20 by cayamash          #+#    #+#             */
+/*   Updated: 2024/10/29 08:49:20 by cayamash         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		s1_len;
+	int		s2_len;
+	int		join_len;
+	char	*s_join;
+
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	join_len = s1_len + s2_len + 1;
+	s_join = malloc(join_len * sizeof(char));
+	if (!s_join)
+		return (NULL);
+	ft_strlcpy(s_join, s1, s1_len + 1);
+	ft_strlcat(s_join, s2, join_len);
+	return (s_join);
+}
